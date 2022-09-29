0e578400d421fdb142af758e71b52b904a1ce081
eeisler <eeislerr@yandex.ru>
2022-09-29 10:33:28 +0500

    Complete pz-6

---
+++ pz-6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 15;
+            int counter = 1;
+
+            while (counter <= n) 
+            {
+                int result = Convert.ToInt32(Math.Pow(counter, 2));
+                if (result < n)
+                {
+                    Console.WriteLine(result);
+                    counter++;
+                }
+                else 
+                {
+                    break;
+                }
+                
+            }
+        }
+    }
+}
