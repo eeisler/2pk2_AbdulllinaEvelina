c39e2ce60476bf5d7a6de663422ad7564c38317a
eeisler <eeislerr@yandex.ru>
2022-10-06 10:33:46 +0500

    Complete pz-7

---
+++ pz-7/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            int sum = 0;
+
+            Console.Write("Enter 10 numbers: ");
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (array[i] == 0)
+                {
+                    break;
+                }
+
+                sum += array[i];
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
