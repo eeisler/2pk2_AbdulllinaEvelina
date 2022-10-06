6f5182b2ffcbd05c2b24bc91be338e2fa37cf275
eeisler <eeislerr@yandex.ru>
2022-10-06 11:18:40 +0500

    Completed additional task pz-7

---
+++ pz-7-add/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace pz_7_add
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] array = new int[100];
+
+            for (int i = 0; i < array.Length; i++)
+            { 
+                array[i] = rnd.Next(0, 20);
+                Console.Write(array[i] + " ");
+            }
+
+            Array.Sort(array);
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] != array[i + 1])
+                {
+                    if (array[i + 1] != array[i + 2])
+                    {
+                        Console.WriteLine($"\n{array[i + 1]} is unique");
+                    }
+                }  
+            }
+        }
+    }
+}
