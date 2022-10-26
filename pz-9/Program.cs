00a1c3fa2c161d8fb7e763fe553c270de18413da
Evelina Eisler <eeisler@yandex.ru>
2022-10-27 00:35:10 +0500

    Completed pz-9

--- pz-9/Program.cs
+++ pz-9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace pz_9
 {
@@ -8,11 +9,14 @@ namespace pz_9
 		{
 			int[][] nums = new int[10][];
 			Random rnd = new Random();
-			
-			for(int i = 0; i < 10; i++)
+
+            Console.WriteLine("Task 1-2: ");
+
+            for (int i = 0; i < 10; i++)
 			{
 				int length = rnd.Next(3, 10);
 				int[] mun = new int[length];
+
 				for(int j = 0; j < length; j++)
 				{
 					mun[j] = rnd.Next(3, 10);
@@ -29,22 +33,25 @@ namespace pz_9
 				}
 				Console.WriteLine();
 			}
-			
-			int[] lasts = new int[10];
+
+            Console.WriteLine("\nTask 3: ");
+
+            int[] lasts = new int[10];
 			
 			for(int i = 0; i < 10; i++)
 			{
 				lasts[i] = nums[i][nums[i].Length-1];
 			}
 			
-			Console.WriteLine();
-			
 			foreach(int i in lasts)
 				{
 					Console.Write(i + " ");
-				}
-				
-			int[] max = new int[10];
+                    Console.WriteLine();
+                }
+
+            Console.WriteLine("\nTask 4: ");
+
+            int[] max = new int[10];
 			
 			for(int i = 0; i < 10; i++)
 			{
@@ -58,14 +65,75 @@ namespace pz_9
 					}
 				}
 				max[i] = maxValue;
-			}
-			Console.WriteLine();
+            }
+
 			foreach(int i in max)
 				{
 					Console.Write(i + " ");
+                    Console.WriteLine();
+                }
+
+            Console.WriteLine("\nTask 5: ");
+
+			for (int i = 0; i < 10; i++)
+			{
+				int MAX = max[i];
+				int first = nums[i][0];
+				for (int j = 0; j < nums[i].Length; j++)
+				{
+					if (nums[i][j] == MAX)
+					{
+						nums[i][0] = MAX;
+						nums[i][j] = first;
+					}
 				}
-			
-			Console.ReadKey();
+			}
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < nums[i].Length; j++)
+                {
+					Console.Write(nums[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nTask 6");
+
+            int val = 0;
+            for (int a = 0; a < 10; a++)
+            {
+                for (int i = 0, j = nums[a].Length - 1; i < j; i++, j--)
+                {
+                    val = nums[a][i];
+                    nums[a][i] = nums[a][j];
+                    nums[a][j] = val;
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < nums[i].Length; j++)
+                {
+                    Console.Write(nums[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nTask 7");
+
+			int[] average = new int[10];
+
+			for (int i = 0; i < 10; i++) 
+			{
+                int sum = 0;
+                for (int j = 0; j < nums[i].Length; j++) 
+				{
+                    sum += nums[i][j];
+                }
+				average[i] = sum / nums[i].Length;
+				Console.WriteLine(average[i]);
+            }
+            Console.ReadKey();
 		}
 	}
 }
