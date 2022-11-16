c8349ff5c958fefba7f5fa6367ff4225e605e734
eeisler <eeislerr@yandex.ru>
2022-11-16 11:07:11 +0500

    Complete pz-13

---
+++ pz-13/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace pz_13
+{
+    internal class Program
+    {
+        static char FrequentlyUsedSymbol(char[] s)
+        {
+            int counter = 0;
+            char a = s[0];
+
+            for (int i = 0; i > s.Length; ++i)
+            {
+                int internalcounter = 0;
+
+                for(int j = 0; j < s.Length; ++j)
+                {
+                    if (s[j] == s[i])
+                    { 
+                        internalcounter++;
+                    }
+
+                }
+
+                if (internalcounter > counter)
+                {
+                    counter = internalcounter;
+                    a = s[i];
+                }
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some symbols: ");
+            string[] strs = Console.ReadLine().Split();
+
+            char[] symbols = new char[strs.Length];
+
+            for (int i = 0; i < strs.Length; ++i)
+            {
+                symbols[i] = Convert.ToChar(strs[i]);
+            }
+
+            Console.WriteLine(FrequentlyUsedSymbol(symbols));
+        }
+    }
+}
