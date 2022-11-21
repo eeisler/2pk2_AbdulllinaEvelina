73992230c7a91d28ae6169cee7cf6ae983a86375
Evelina Eisler <eeisler@yandex.ru>
2022-11-21 20:36:59 +0500

    Complete pz-14

--- pz-14/Program.cs
+++ pz-14/Program.cs
@@ -28,22 +28,48 @@
             }
             if (A < B)
             {
-                Console.WriteLine(A);
+                Console.WriteLine($"{A}"); 
                 return AllNumsAtoB(A + 1, B);
+            }
+            if (A > B)
+            {
+                Console.WriteLine(A); 
+                return AllNumsAtoB(A - 1, B);
             }
             else 
             {
                 return 0;
             }
-            
         }
+        /* Задание 4. Описать рекурсивную функцию Palindrom(S) логического типа, возвращающую True, если
+        строка S является палиндромом (то есть читается одинаково слева направо и справа налево),
+        и False в противном случае. Оператор цикла в теле функции не использовать. Вывести
+        значения функции Palindrom для пяти данных строк.*/
 
+       
+        static string Reverse(string str)
+        {
+            return str.Length < 2 ? str : Reverse(str.Substring(1)) + str[0];  
+        }
+    
+        static bool Palindrom(string a)
+        {
+            if(Reverse(a) == a)
+                return true;
+           
+            else
+                return false;
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(ArifmeticProgression(1, -5, 3));
-            Console.WriteLine(Math.Round(GeometricProgression(1, 1, 3)));
+            Console.WriteLine($"Arithmetic progression: {ArifmeticProgression(1, -5, 3)}");
+            Console.WriteLine($"Geometric progression: {Math.Round(GeometricProgression(1, 1, 3), 2)}");
             Console.WriteLine(AllNumsAtoB(1, 10));
+            Console.WriteLine(AllNumsAtoB(10, 1));
+            Console.Write("Enter the string: ");
+            Console.WriteLine(Palindrom(Console.ReadLine()));
+            Console.ReadKey();
         }
     }
 }
