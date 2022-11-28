4071ce7439b349de3f3c439e145709c6a5dfeec2
eeisler <eeislerr@gmail.com>
2022-11-28 13:28:13 +0500

    Complete pz-15

---
+++ pz-15/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace pz_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\work\new.txt";
+
+            Console.Write("Enter the string that needed to be found: ");
+            string wantedSubstr = Console.ReadLine();
+
+            string[] allStrFromFile = File.ReadAllLines(filePath);
+
+            // обход по каждой строке файлов
+            for (int i = 0; i < allStrFromFile.Length; i++) 
+            {
+                if (allStrFromFile[i].Contains(wantedSubstr)) // если строка содержит подстроку
+                {
+                    Console.WriteLine(allStrFromFile[i]);
+                }
+            }
+
+        }
+    }
+}
