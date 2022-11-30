78bd596f9db0abffb884b04b96846bacbeb7e4f1
eeisler <eeislerr@gmail.com>
2022-11-30 11:00:58 +0500

    Complete pz-16

---
+++ pz-16/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace pz_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"D:\work\new\temp.txt";
+
+            string[] allStrFromFile = File.ReadAllLines(file);
+
+            Console.WriteLine($"Count of the lines: {allStrFromFile.Length}");
+
+            int count_of_the_words = 0;
+            for (int i = 0; i < allStrFromFile.Length; i++)
+            {
+                count_of_the_words += allStrFromFile[i].Split(' ').Length;
+            }
+            Console.WriteLine($"Count of the words: {count_of_the_words}");
+        }
+    }
+}
