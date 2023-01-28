0a193b45be16a59e7b3950c598b4fab4b6904e6e
eeisler <eeislerr@gmail.com>
2023-01-28 13:37:08 +0500

    Complete pz-21

---
+++ pz-21/FractionalNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class FractionalNumber
+    {
+        private string sign;
+        private string dividend;
+        private string divider;
+
+        public FractionalNumber()
+        {
+            this.sign = " ";
+            this.dividend = "0";
+            this.divider = "0";
+        }
+        public FractionalNumber(string sign)
+        {
+            this.sign = sign;
+            this.dividend = "0";
+            this.divider = "0";
+        }
+        public FractionalNumber(string sign, string divider)
+        {
+            this.sign = sign;
+            this.dividend = "0";
+            this.divider = divider;
+        }
+
+        public FractionalNumber(string sign, string dividend, string divider)
+        {
+            this.sign = sign;
+            this.dividend = dividend;   
+            this.divider = divider;
+        }
+        public string GetNumber { get { return $"Result: {this.sign} {this.dividend}/{this.divider}"; } }
+    }
+}
---
+++ pz-21/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace pz_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the sign: ");
+            string sign = Console.ReadLine();
+
+            Console.Write("Enter the dividend: ");
+            string dividend = Console.ReadLine();
+
+            Console.Write("Enter the divider: ");
+            string divider = Console.ReadLine();
+
+            FractionalNumber a = new FractionalNumber();
+            FractionalNumber b = new FractionalNumber(sign);
+            FractionalNumber c = new FractionalNumber (sign, divider);
+            FractionalNumber d = new FractionalNumber(sign, dividend, divider);
+
+            if (sign == "" & dividend == "" & divider == "")
+            {
+                Console.WriteLine(a.GetNumber);
+            }
+
+            else if (dividend == "" & divider == "")
+            {
+                Console.WriteLine(b.GetNumber);
+            }
+
+            else if (dividend == "")
+            {
+                Console.WriteLine(c.GetNumber);
+            }
+
+            else
+            {
+                Console.WriteLine(d.GetNumber);
+            }
+           
+        }
+    }
+}
