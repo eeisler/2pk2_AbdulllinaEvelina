c9acd97e1e6f5f4be82ffaf175f8b3273fcbab62
eeisler <eeislerr@gmail.com>
2023-02-09 11:34:22 +0500

    Complete pz-22

---
+++ pz-22/FractionalNumbers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_22
+{
+    internal class FractionalNumbers
+    {
+        public static int positiveCounter = 0;
+        public static int negativeCounter = 0;
+
+        private string sign;
+        private int dividend;
+        private int divider = 1;
+
+        public string Sign
+        {
+            get { return sign; }
+            set
+            {
+                if (value == "+" || value == "-")
+                {
+                    sign = value;
+                }
+                else
+                {
+                    sign = "+";
+                }
+            }
+        }
+
+        public int Divider
+        {
+            set 
+            {
+                if (value != 0)
+                {
+                    divider = value;
+                }
+                else if (value == 0)
+                {
+                    Console.WriteLine($"Divider can't be 0\nDivider equals {this.divider}");
+                }
+            }
+        }
+
+        public string GetNumber { get { return $"Result: {this.sign}({this.dividend}/{this.divider})"; } }
+
+        public FractionalNumbers(string sign, int dividend, int divider)
+        {
+            this.Sign = sign;
+            this.dividend = dividend;
+            this.Divider = divider;
+            if (this.sign == "+")
+            {
+                positiveCounter++;
+            }
+            else
+            {
+                negativeCounter++;
+            }
+        }           
+    }
+}
---
+++ pz-22/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace pz_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "/continue";
+
+            while(word != "/stop")
+            {
+                Console.Write("Enter the sign: ");
+                string sign = Console.ReadLine();
+
+                EnterDividend:
+                    Console.Write("Enter the dividend: ");
+                    int dividend = 1;
+                    try
+                    {
+                        dividend = Convert.ToInt32(Console.ReadLine());
+                    } 
+                    catch 
+                    {
+                        Console.WriteLine("Error: wrong dividend");
+                        goto EnterDividend;
+                    }
+
+                EnterDivider:
+                    Console.Write("Enter the divider: ");
+                    int divider = 1;
+                    try
+                    {
+                        divider = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error: wrong divider");
+                        goto EnterDivider;
+                    }
+
+                FractionalNumbers d = new FractionalNumbers(sign, dividend, divider);
+
+                Console.WriteLine(d.GetNumber);
+
+                Console.Write("Enter the command(/continue or /stop): ");
+                word = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Positive numbers count: {FractionalNumbers.positiveCounter}");
+            Console.WriteLine($"Negative numbers count: {FractionalNumbers.negativeCounter}");
+        }
+    }
+}
