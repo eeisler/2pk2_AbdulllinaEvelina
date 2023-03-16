7644577b804de17d99ff47415d85715d650c5cf1
eeisler <eeislerr@gmail.com>
2023-03-16 13:27:38 +0500

    Uncompleted pz-26

---
+++ pz-26-TextEditor/FileFunctional.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_26_TextEditor
+{
+    //CreateFile(), SaveFile(), OpenFile() и DeleteFile()
+    internal class FileFunctional
+    {
+        public void CreateFile()
+        {
+
+        }
+        public void SaveFile()
+        {
+
+        }
+        public void OpenFile()
+        {
+
+        }
+        public void DeleteFile()
+        {
+
+        }
+    }
+}
